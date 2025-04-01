2385949f2e350b87bae5715b6344475b9976d06b
Daylily-Zeleen <daylily-zeleen@foxmail.com>
2025-04-01 15:57:24 +0800

    更新demo

--- demo/dragonbones_demo/demo.gd
+++ demo/dragonbones_demo/demo.gd
@@ -1,24 +1,21 @@
 extends Node
 
 
-@onready var dragonbones :DragonBones = $DragonBones
+@onready var armature :DragonBonesArmatureDisplay = $DragonBonesArmatureDisplay
 @onready var animation_process_mode_option_btn :OptionButton = %AnimationProcessModeOptionBtn
 @onready var animation_option_btn :OptionButton = %AniamtionOptionBtn
-
-
-@onready var _armature :DragonBonesArmature = dragonbones.armature
 
 
 # Setup UI to control DragonBonesArmature's properties.
 func _ready() -> void:
 	# AnimationCallbackModeProcess
-	for e: String in ClassDB.class_get_enum_constants(&"DragonBones", &"AnimationCallbackModeProcess"):
+	for e: String in ClassDB.class_get_enum_constants(&"DragonBonesArmatureDisplay", &"AnimationCallbackModeProcess"):
 		animation_process_mode_option_btn.add_item(e.rsplit("_", false, 1)[1])
-		animation_process_mode_option_btn.set_item_metadata(animation_process_mode_option_btn.item_count - 1, DragonBones[e])
+		animation_process_mode_option_btn.set_item_metadata(animation_process_mode_option_btn.item_count - 1, DragonBonesArmatureDisplay[e])
 
 	animation_process_mode_option_btn.item_selected.connect(_on_process_mode_option_btn_item_selected)
 	for idx in range(animation_process_mode_option_btn.item_count):
-		if animation_process_mode_option_btn.get_item_metadata(idx) == DragonBones.ANIMATION_CALLBACK_MODE_PROCESS_IDLE:
+		if animation_process_mode_option_btn.get_item_metadata(idx) == DragonBonesArmatureDisplay.ANIMATION_CALLBACK_MODE_PROCESS_IDLE:
 			animation_process_mode_option_btn.select(idx)
 			_on_process_mode_option_btn_item_selected(idx)
 			break
@@ -26,7 +23,7 @@ func _ready() -> void:
 	# Animation
 	animation_option_btn.add_item("[reset]")
 	animation_option_btn.set_item_metadata(0, "")
-	for anim in _armature.get_animations():
+	for anim in armature.get_animations():
 		animation_option_btn.add_item(anim)
 		animation_option_btn.set_item_metadata(animation_option_btn.item_count - 1, anim)
 
@@ -35,44 +32,44 @@ func _ready() -> void:
 	_on_animation_option_btn_item_selected(0)
 
 	# Flip
-	%FlipXCheck.toggled.connect(func(toggled: bool): _armature.flip_x = toggled)
-	%FlipYCheck.toggled.connect(func(toggled: bool): _armature.flip_y = toggled)
+	%FlipXCheck.toggled.connect(func(toggled: bool): armature.flip_x = toggled)
+	%FlipYCheck.toggled.connect(func(toggled: bool): armature.flip_y = toggled)
 	%FlipXCheck.button_pressed = false
 	%FlipYCheck.button_pressed = false
 
 	# Debug
-	%DebugCheck.toggled.connect(func(toggled: bool): dragonbones.debug = toggled)
+	%DebugCheck.toggled.connect(func(toggled: bool): armature.debug = toggled)
 	%DebugCheck.button_pressed = false
 
 	# Active
-	%ActiveCheck.toggled.connect(func(toggled: bool): dragonbones.active = toggled)
+	%ActiveCheck.toggled.connect(func(toggled: bool): armature.active = toggled)
 	%ActiveCheck.button_pressed = false
 
 	# Time Scale
-	%TimeScaleSpinBox.value_changed.connect(func(value: float): dragonbones.animation_time_scale = value)
+	%TimeScaleSpinBox.value_changed.connect(func(value: float): armature.animation_time_scale = value)
 	%TimeScaleSpinBox.value = 1.0
 	
 	# Manual Advance
 	%AdvanceBtn.pressed.connect(_on_advance_btn_pressed)
 
 func _on_process_mode_option_btn_item_selected(index: int) -> void:
-	dragonbones.animation_callback_mode_process = animation_process_mode_option_btn.get_item_metadata(index)
-	%AdvanceUI.visible = dragonbones.animation_callback_mode_process == DragonBones.ANIMATION_CALLBACK_MODE_PROCESS_MANUAL
+	armature.animation_callback_mode_process = animation_process_mode_option_btn.get_item_metadata(index)
+	%AdvanceUI.visible = armature.animation_callback_mode_process == DragonBonesArmatureDisplay.ANIMATION_CALLBACK_MODE_PROCESS_MANUAL
 
 
 func _on_animation_option_btn_item_selected(index: int) -> void:
 	var anim :String = animation_option_btn.get_item_metadata(index) as String
-	if anim == _armature.current_animation:
+	if anim == armature.current_animation:
 		return
 
-	_armature.stop(_armature.current_animation, true)
+	armature.stop(armature.current_animation, true)
 	if not anim.is_empty():
-		_armature.play(anim, -1)
+		armature.play(anim, -1)
 		#dragonbones.play()
 
 
 func _on_advance_btn_pressed() -> void:
-	assert(dragonbones.animation_callback_mode_process == DragonBones.ANIMATION_CALLBACK_MODE_PROCESS_MANUAL)
+	assert(armature.animation_callback_mode_process == DragonBonesArmatureDisplay.ANIMATION_CALLBACK_MODE_PROCESS_MANUAL)
 	var delta :float = %AdvanceTime.value
-	dragonbones.advance(delta)
+	armature.advance(delta)
 	
